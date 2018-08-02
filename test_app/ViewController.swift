663db725565f10b3c4eaa7ddcc9600f9b47b19f8
vvzhukov <zhuko.v.italy@gmail.com>
2018-08-02 16:35:00 +0300

    Initial Commit

---
+++ test_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_app
+//
+//  Created by Vitaly Zhukov on 02/08/2018.
+//  Copyright © 2018 Vitaly Zhukov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
